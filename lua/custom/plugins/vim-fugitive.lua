cf86aa703f7506a7cd8ddc5a2d9ea6029ca95949
BernatRubio <bernat.rubio.oliete@gmail.com>
2025-02-12 22:43:14 +0100

    Added some keybindings for fugitive

--- lua/custom/plugins/vim-fugitive.lua
+++ lua/custom/plugins/vim-fugitive.lua
@@ -2,6 +2,8 @@ return {
   'tpope/vim-fugitive',
   cmd = 'Git',
   keys = {
-    { '<leader>g', ':Git<CR>', desc = '[G]it', silent = true },
+    { '<leader>gg', ':Git<CR>', desc = '[G]it', silent = true },
+    { '<leader>gf', ':Git fetch<CR>', desc = '[G]it [F]etch', silent = true },
+    { '<leader>gl', ':Git pull<CR>', desc = '[G]it pul[L]', silent = true },
   },
 }
