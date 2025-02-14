a40c63741b8311320b1233a2830e3f0a347be577
BernatRubio <bernat.rubio.oliete@gmail.com>
2025-02-14 17:09:36 +0100

    Remove vim-fugitive plugin

--- lua/custom/plugins/vim-fugitive.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'tpope/vim-fugitive',
-  cmd = 'Git',
-  keys = {
-    { '<leader>gg', ':Git<CR>', desc = '[G]it', silent = true },
-    { '<leader>gf', ':Git fetch<CR>', desc = '[G]it [F]etch', silent = true },
-    { '<leader>gl', ':Git pull<CR>', desc = '[G]it pul[L]', silent = true },
-  },
-}
